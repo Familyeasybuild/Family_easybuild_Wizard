484d8a3b56d689bb9240de4d9d56c2d2494d558d
simon <147948148+Familyeasybuild@users.noreply.github.com>
2026-05-31 19:27:33 +0100

    Add files via upload

---
+++ uservar.py
@@ -0,0 +1,11 @@
+'''#####-----Build File-----#####'''
+buildfile = 'http://CHANGEME'
+
+'''#####-----Videos File-----#####'''
+videos_url = 'http://CHANGEME'
+
+'''#####-----Notifications File-----#####'''
+notify_url  = 'http://CHANGEME'
+
+'''#####-----Excludes-----#####'''
+excludes  = ['plugin.video.whatever']
